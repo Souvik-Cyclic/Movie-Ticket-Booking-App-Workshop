5a8ca683a9221330a6e02f3fd79ba1ec049664ae
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-18 01:55:59 +0530

    enhances look & feel of sent emails

--- project/server/utils/emailSender.js
+++ project/server/utils/emailSender.js
@@ -1,30 +1,30 @@
 const nodemailer = require('nodemailer');
-const fs = require("fs");
+const fs = require("fs").promises;
 const dotenv = require("dotenv");
 const path = require("path");
 dotenv.config();
 
 const { SMTP_HOST, SMTP_PORT, SMTP_MAIL, SMTP_PASSWORD } = process.env;
 
-function replaceContent(content, creds) {
+async function replaceContent(content, creds) {
     let allkeysArr = Object.keys(creds);
     allkeysArr.forEach(function (key) {
         content = content.replace(`#{${key}}`, creds[key]);
-    })
+    });
     return content;
 }
 
-async function EmailHelper(templateName, receiverEmail, creds) {
+async function EmailHelper(templateName, receiverEmail, creds, subject = 'Mail From Book My Show') {
     try {
         const templatePath = path.join(__dirname, "email_templates", templateName);
-        let content = await fs.promises.readFile(templatePath, "utf-8");
+        let content = await fs.readFile(templatePath, "utf-8");
 
         const emailDetails = {
             to: receiverEmail,
             from: SMTP_MAIL,
-            subject: 'RESET OTP',
+            subject: subject, // Use dynamic subject based on parameter or default to 'Default Subject'
             text: `Hi ${creds.name}, this is your reset OTP: ${creds.otp}`,
-            html: replaceContent(content, creds),
+            html: await replaceContent(content, creds),
         };
 
         const transportDetails = {
@@ -40,7 +40,7 @@ async function EmailHelper(templateName, receiverEmail, creds) {
         await transporter.sendMail(emailDetails);
         console.log("Email Sent Successfully.");
     } catch (err) {
-        console.log("Email not sent. Check Error Below:")
+        console.log("Email not sent. Check Error Below:");
         console.log(err);
     }
 }
