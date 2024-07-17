6bcbfe3be924292f00d46a7aeec8d47a417432ec
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-17 23:06:10 +0530

    minor changes

--- project/server/config/dbConfig.js
+++ project/server/config/dbConfig.js
@@ -1,12 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-mongoose.connect(process.env.DATABASE_URL)
+mongoose.connect(process.env.DATABASE_URL);
 
-const connection = mongoose.connection
+const connection = mongoose.connection;
 
-connection.on('connected' , ()=>{
-    console.log('Connection Successful')
-})
-connection.on('error' , ()=>{
-    console.log('Failed to connect to database server.')
-})
+connection.on("connected", () => {
+  console.log("Connection Successful");
+});
+connection.on("error", () => {
+  console.log("Failed to connect to database server.");
+});
