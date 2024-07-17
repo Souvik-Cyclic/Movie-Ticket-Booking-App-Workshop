b1d095c193347750c42a0435085766e7a2aa4fc6
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-17 19:03:31 +0530

    add a page, which not authorized viewer sees when then try to access different role menu.

---
+++ project/client/src/pages/Unauthorized.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UnAuthorized = () => {
+    return (
+        <div>
+            <h1>Un-Authorized</h1>
+            <p>You do not have permission to access this page.</p>
+        </div>
+    );
+};
+
+export default UnAuthorized;
