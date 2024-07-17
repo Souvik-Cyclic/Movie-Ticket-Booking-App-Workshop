33b0df2a6321427f922f962206d244838d0c4c64
Souvik Kumar <souvik.cyc@gmail.com>
2024-07-18 00:29:50 +0530

    add search box in admin portal

--- project/client/src/pages/Admin/MovieList.js
+++ project/client/src/pages/Admin/MovieList.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useState } from "react";
-
-import { Button, Table } from "antd";
+import { Button, Table, Input, Row, Col } from "antd";
 import MovieForm from "./MovieForm";
+import DeleteMovieModal from "./DeleteMovieModal";
 import { hideLoading, showLoading } from "../../redux/loaderSlice";
 import { getAllMovies } from "../../calls/movies";
 import { useDispatch } from "react-redux";
 import moment from "moment";
-import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
-import DeleteMovieModal from "./DeleteMovieModal";
+import { EditOutlined, DeleteOutlined, SearchOutlined } from "@ant-design/icons";
 
 function MovieList() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,10 +14,9 @@ function MovieList() {
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [formType, setFormType] = useState("add");
   const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [searchText, setSearchText] = useState("");
 
   const dispatch = useDispatch();
-
-  console.log(selectedMovie);
 
   const getData = async () => {
     dispatch(showLoading());
@@ -32,7 +30,7 @@ function MovieList() {
         return { ...item, key: `movie${item._id}` };
       })
     );
-    console.log(movies);
+
     dispatch(hideLoading());
   };
 
@@ -114,20 +112,39 @@ function MovieList() {
     getData();
   }, []);
 
+  const handleSearch = (value) => {
+    setSearchText(value.toLowerCase());
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchText)
+  );
+
   return (
     <>
-      <div className="d-flex justify-content-end">
-        <Button
-          onClick={() => {
-            setIsModalOpen(true);
-            setFormType("add");
-          }}
-        >
-          Add Movie
-        </Button>
-      </div>
+      <Row justify="space-between" align="middle" style={{ marginBottom: 16 }}>
+        <Col flex="auto">
+          <Input
+            placeholder="Search movies"
+            prefix={<SearchOutlined />}
+            onChange={(e) => handleSearch(e.target.value)}
+            style={{ width: "100%", maxWidth: 400 }}
+          />
+        </Col>
+        <Col>
+          <Button
+            onClick={() => {
+              setIsModalOpen(true);
+              setFormType("add");
+            }}
+          >
+            Add Movie
+          </Button>
+        </Col>
+      </Row>
 
-      <Table dataSource={movies} columns={tableHeadings} />
+      <Table dataSource={filteredMovies} columns={tableHeadings} />
+
       {isModalOpen && (
         <MovieForm
           isModalOpen={isModalOpen}
